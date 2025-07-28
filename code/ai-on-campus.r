b15b07520f849c60179518a9e0fb6462ccab3639
Tristan-Raz <razotet@berea.edu>
2025-07-28 09:17:39 -0400

    fixes the file reference name to not use spaces

--- code/ai-on-campus.r
+++ code/ai-on-campus.r
@@ -1,4 +1,4 @@
 library(readxl)
-download.file("https://raw.githubusercontent.com/pearcej/intro2datasci/main/code/AI%20On-Campus%20Research%20Survey%20(Responses).xlsx", tmp <- tempfile(fileext = ".xlsx"))
+download.file("https://raw.githubusercontent.com/pearcej/intro2datasci/main/code/ai-on-campus.xlsx", tmp <- tempfile(fileext = ".xlsx"))
 aiOnCampus <- read_excel(tmp)
 
